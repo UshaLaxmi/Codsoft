ce696c9b7cb471a99d9cb16ddf3346cc36ea30b0
UshaLaxmi <laxmiusha43@gmail.com>
2024-11-23 15:24:54 +0530

    Add files via upload

---
+++ task3/script.js
@@ -0,0 +1,26 @@
+let currentInput = "";
+
+function appendNumber(number) {
+  currentInput += number;
+  document.getElementById('display').value = currentInput;
+}
+
+function appendOperator(operator) {
+  currentInput += ` ${operator} `;
+  document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+  currentInput = "";
+  document.getElementById('display').value = currentInput;
+}
+
+function calculateResult() {
+  try {
+    currentInput = eval(currentInput).toString();
+    document.getElementById('display').value = currentInput;
+  } catch (error) {
+    document.getElementById('display').value = "Error";
+    currentInput = "";
+  }
+}
